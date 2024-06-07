a9321894dff351b1db12993d7dd48fe57e1497f7
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2024-06-06 23:03:48 -0300

    Update subMenu.java

--- subMenu.java
+++ subMenu.java
@@ -6,16 +6,11 @@ public class subMenu {
 
     // Funções "Globais"
     // Para controle geral do parque
-
     /*
      * 6) O sistema deverá permitir ao operador consultar o faturamento de um determinado mês/ano, somando os valores dos ingressos dos respectivos dias.
      * 7) O sistema deverá permitir ao operador consultar, para uma determinada data, todas as atrações do parque e a quantidade de visitantes que utilizou cada uma delas, ordenadas da mais visitada à menos visitada.
      * 8) Duas funcionalidades a mais, à escolha do grupo.
     */
-
-
-
-    
 
     public final ArrayList<RegistrosDia> encerraDia() {
         ArrayList<RegistrosDia> resultadosDia = new ArrayList<>();
@@ -26,7 +21,6 @@ public class subMenu {
         return resultadosDia;
     }
     
-
     private final ArrayList<registrosDia> faturamentoDia() {
         ArrayList<registrosDia> registrosDia = new ArrayList<>();
         System.out.println("Consultando faturamento do dia " + getData());
@@ -36,13 +30,9 @@ public class subMenu {
         return faturamento;
     }
 
-
     // encerraDiaResultados()
     // ArraysLists : diaArray, atracoesArray, visitantesArray, ingressosArray
-
     // isIngressoValido()
-
-
 
     private static String dataStart = DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDateTime.now());
     private static String dataAtual = dataStart;
@@ -57,6 +47,17 @@ public class subMenu {
         dataAtual = incrementaData.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         Ingresso.encerraDia(1); // Reseta controleIngresso ao finalizar o dia
     }
-    
+
+    // Encerra dia
+
+    public void encerrarDia(String data) {
+        double totalFaturamento = 0.0;
+        for (RegistroGeral registro : registroGeral) {
+            if (registro.getData().equals(data)) {
+                totalFaturamento += registro.getFaturamento().getValor();
+            }
+        }
+        System.out.println("Faturamento do dia " + data + ": " + totalFaturamento);
+    }
 }
 
